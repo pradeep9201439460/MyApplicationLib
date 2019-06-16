7cfa2d42506e0b3da9309c03a0bde1aff742eee8
Pradeep Kumar Singh <p9201439460@gmail.com>
2019-06-16 23:47:22 +0530

    Add Log.d

--- logutil/src/main/java/com/gonuclei/logutil/LogDebug.java
+++ logutil/src/main/java/com/gonuclei/logutil/LogDebug.java
@@ -5,6 +5,6 @@ import android.util.Log;
 public class LogDebug {
     private static final String TAG="LogDebug.java";
     public static void d(String message){
-        Log.d(TAG, "d: "+message);
+        Log.d(TAG,message);
     }
 }
