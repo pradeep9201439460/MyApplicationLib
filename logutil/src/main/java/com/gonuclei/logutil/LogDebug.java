2dcfb10436268c178e1aab97b0ebd91c911000a4
Pradeep Kumar Singh <p9201439460@gmail.com>
2019-06-16 23:40:59 +0530

    Initial commit

---
+++ logutil/src/main/java/com/gonuclei/logutil/LogDebug.java
@@ -0,0 +1,10 @@
+package com.gonuclei.logutil;
+
+import android.util.Log;
+
+public class LogDebug {
+    private static final String TAG="LogDebug.java";
+    public static void d(String message){
+        Log.d(TAG, "d: "+message);
+    }
+}
